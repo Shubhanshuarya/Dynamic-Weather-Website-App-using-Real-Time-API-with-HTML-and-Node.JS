afd318dc18c223a69c7ae2f69fe776cca5ed57c7
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-06-09 10:00:29 +0530

    Complete Dynamic NodeJS and HTML Weather Application

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "http://api.openweathermap.org/data/2.5/weather?q=London&appid=38dc9b20118dc0d9891c38734f010730"
+    )
+      .on("data", function (chunk) {
+        const orgData = JSON.parse(chunk);
+        const arrData = [orgData];
+        // console.log(arrData[0].main);
+        const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+        res.write(realTimeData);
+      })
+      .on("end", function (err) {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }else {
+    res.end("File not found");
+  }
+});
+
+server.listen(3000, "127.0.0.1");
